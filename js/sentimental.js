6c1316db12496cc3da908760fd9efd941d2983ca
toberge <toberge@stud.ntnu.no>
2019-08-23 15:14:34 +0200

    added switchers for demonstrating functionality w/o access to sentiment service

--- js/sentimental.js
+++ js/sentimental.js
@@ -1,55 +1,4 @@
-"use strict";
-
-/*var ws= new WebSocket("ws://theURL");
-ws.onmessage= event=> {
-    console.log("WS event: " + event.data);
-    document.getElementById("sendme").innerText = event.data.stringify();
-};*/
-// Manipuler HTML med innhold fra event.data};
-
-/*const moodString = (response) => {
-    switch (response) {
-        case "0":
-            return "terrible";
-        case "1":
-            return "awful";
-        case "2":
-            return "okay";
-        case "3":
-            return "good";
-        case "4":
-            return "magnificent";
-        default:
-            return "undefined";
-    }
-};
-
-const url = 'http://bigdata.stud.iie.ntnu.no/sentiment/webresources/sentiment?api-key=Happy%21%21%21';
-// api-key does not seem to be necessary after all, wow
-
-const fetchMood = (data) => {
-
-    // let mood = "defiantly bad";
-    let mood = -1;
-
-    fetch(url, {
-        method: 'POST', // or 'PUT'
-        body: JSON.stringify(data)
-    }).then(res => res.json())
-        .then(response => {
-            mood = JSON.stringify(response);
-            mood = moodString(JSON.stringify(response));
-            console.log('Success:', mood);
-            document.getElementById("sendme").innerHTML = `I'm feeling ${mood}.`;
-            //console.log("We have " + mood);
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-
-    //return mood;
-};
-
+/*
 let muchdata = [
     { sentence: 'I am very angry right now, my dear.' },
     { sentence: 'This is a wonderful day.' },
@@ -60,10 +9,13 @@ for (let i = 0; i < muchdata.length; i++) {
     setTimeout(fetchMood(muchdata[i]), 5000 * (i+1));
     /!*let mood = moodString(fetchMood(muchdata[i]));
     console.log(`hello ${mood}`);*!/
-}*/
+}
+*/
 
+(function () {
+"use strict";
 
-const colors = [
+/*const colors = [
     // '#2B3D51',
     '#502332',
     '#E74D3B',
@@ -78,18 +30,31 @@ const moods = [
     'okay',
     'good',
     'magnificent'
-];
+];*/
+
+const states = [
+    { mood: 'terrible',     color: '#502332' },
+    { mood: 'awful',        color: '#E74D3B' },
+    { mood: 'okay',         color: '#FBA446' },
+    { mood: 'good',         color: '#DCD762' },
+    { mood: 'magnificent',  color: '#82C6B5' }
+]; // TODO use this instead of the two arrays, maybe
+
+// let mood = 0;
 
 const url = 'http://bigdata.stud.iie.ntnu.no/sentiment/webresources/sentiment?api-key=Happy%21%21%21';
 // api-key does not seem to be necessary after all, wow
 // note that in urls ! is encoded as %21, Chrome compained but Firefox did not.
 
+// encountered another error now, https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS/Errors/CORSMissingAllowOrigin
+// seems like it's caused by the service being down and sending a response that does not include the correct CORS header(s)
+
 const change = code => {
     if (code < 0 || code > 4) return;
     document.getElementById("sendme")
-        .innerHTML = `You're feeling <strong>${moods[code]}</strong> right now.`;
+        .innerHTML = `You're feeling <strong>${states[code].mood}</strong> right now.`;
     document.getElementsByTagName("body").item(0)
-        .style.backgroundColor = colors[code];
+        .style.backgroundColor = states[code].color;
 };
 
 
@@ -128,19 +93,37 @@ button.addEventListener("click", evt => {
     });
 });
 
+let boxes = document.getElementsByClassName('debug-option');
+
+    for (let i = 0; i < boxes.length; i++) {
+        const box = boxes[i];
+        box.style.backgroundColor = states[i].color;
+        box.addEventListener('click', evt => change(i));
+    }
+
+
 
 let errorField = document.getElementById("error");
 
 fetch(url, {
     method: 'POST', // or 'PUT'
+    header: new Headers({ //TODO check if this works now
+        'Content-Type': 'application/json; charset=utf-8'
+    }),
     body: JSON.stringify({
         sentence: 'I am very sad'
     })
-}).then(res => res.json())
+}).then(res => {
+    if (res.ok) return res.json();
+    else throw new Error('Response was not okay');
+})
 .then(response => {
-    errorField.innerText = 'The service is up & running'
+    errorField.innerText = 'The service is up & running';
+    console.log('Test gave us ' + JSON.stringify(response));
 })
 .catch(error => {
     console.error('Error:', error);
     errorField.innerText = 'Servicen har litt vondt i magen atm';
 });
+
+})();
